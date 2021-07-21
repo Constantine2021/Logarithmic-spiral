0f8712cd211aaea6e342eaea21723df6dfe32bed
Constantine2021 <kosta2016@icloud.com>
2021-07-21 22:48:55 +0300

    project log spiral

---
+++ LibVis3a/MyForm.h
@@ -0,0 +1,135 @@
+#pragma once
+#include <math.h>
+
+namespace LibVis3a {
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	protected:
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(16, 15);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(4);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(645, 452);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->Click += gcnew System::EventHandler(this, &MyForm::pictureBox1_Click);
+			this->pictureBox1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::pictureBox1_Paint);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(696, 481);
+			this->Controls->Add(this->pictureBox1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Margin = System::Windows::Forms::Padding(4);
+			this->MaximizeBox = false;
+			this->Name = L"MyForm";
+			this->Text = L"Вариант 10";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private:
+		int l, b, h, w;
+	private: System::Void pictureBox1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+		h = pictureBox1->Height;
+		w = pictureBox1->Width;
+								
+		e->Graphics->DrawLine(Pens::Black, 0, h / 2, w, h / 2);
+		e->Graphics->DrawLine(Pens::Black, w / 2, 0, w / 2, h);
+		
+		e->Graphics->DrawLine(Pens::Black, w - 25, h / 2 + 8, w, h / 2);
+		e->Graphics->DrawLine(Pens::Black, w - 25, h / 2 - 8, w, h / 2);
+		e->Graphics->DrawLine(Pens::Black, w / 2 - 8, 25, w / 2, 0);
+		e->Graphics->DrawLine(Pens::Black, w / 2 + 8, 25, w / 2, 0);
+		
+		e->Graphics->DrawString("X", this->Font, Brushes::Black, w - 15, h / 2 - 25);
+		e->Graphics->DrawString("Y", this->Font, Brushes::Black, w / 2 - 25, 5);
+		double x1, x2, y1, y2, x, y, mx, my, x0, y0, dx, dt;
+		double pi = 3.1415926;
+		x1 = -5;  //from
+		x2 = 5;   //to
+		dx = 0.002;    //step
+		y1 = pow(2, -5) - sin(pi*pow(2,-5));
+		y2 = pow(2, 5) - sin(pi*pow(2, 5));
+		x = x1;
+		while (x <= x2)
+		{
+			y = pow(2, x) - sin(pi*pow(2, x));
+			if (y < y1) y1 = y;
+			if (y > y2) y2 = y;
+			x += dx;
+		}
+		my = 0.5*h / (y2 - y1);   //mashtab po osi y
+		mx = w / (x2 - x1);  //mashtab po osi x
+		
+		x = x1;
+		y1 = pow(2, x1) - sin(pi*pow(2, x1));
+		x += dx;
+		// График
+
+		while (x <= x2)
+		{
+			y = pow(2, x) - sin(pi*pow(2, x));
+			e->Graphics->DrawLine(Pens::Black, (int)(x1*mx + w / 2), (int)(h / 2 - y1 * my), (int)(x*mx + w / 2), (int)(h / 2 - y * my));
+			x1 = x;
+			y1 = y;
+			x += dx;
+		}
+
+	}
+	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+}
+};
+}
